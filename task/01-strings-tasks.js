1c491fabbe52f8330a4f321934c2ea47fc8863bb
Vlad Mironchuk <miron_1512@tut.by>
2016-03-17 22:55:13 +0300

    Implement function 'getRectangleString'

--- task/01-strings-tasks.js
+++ task/01-strings-tasks.js
@@ -201,7 +201,27 @@ function extractEmails(str) {
  *
  */
 function getRectangleString(width, height) {
-    throw new Error('Not implemented');
+    var result = "";
+    for (let i = 0; i < height; i++) {
+        for (let j = 0; j < width; j++) {
+            if (i == 0 && j == 0)
+                result += "┌";
+            if (i == 0 && j == width - 1)
+                result += "┐";
+            if (i == height - 1 && j == 0)
+                result += "└";
+            if (i == height - 1 && j == width - 1)
+                result += "┘";
+            if ((i == 0 || i == height - 1) && (j != 0 && j != width - 1))
+                result += "─";
+            if ((i != 0 && i != height - 1) && (j == 0 || j == width - 1))
+                result += "│";
+            if ((i != 0 && i != height - 1) && (j != 0 && j != width - 1))
+                result += " ";
+        }
+        result += "\n";
+    }
+    return result;
 }
 
 
